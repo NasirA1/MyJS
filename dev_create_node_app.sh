06076d18ba332c083e3f9bfa7cb76a98da72888f
nas <Nasir.Ahmad@helvar.com>
2017-07-07 16:52:01 +0100

    added sample first test

--- dev_create_node_app.sh
+++ dev_create_node_app.sh
@@ -17,10 +17,24 @@ echo 'initialising node app'
 npm init -y
 
 echo 'installing test frameworks'
-npm install mocha chai sinon --save-dev
+#npm install mocha chai sinon --save-dev
 
 echo 'Update "test" path in package.json'
 sed -i 's/"test": "echo \\"Error: no test specified\\" && exit 1"/"test": ".\/node_modules\/.bin\/mocha --reporter spec"/g' package.json
+
+echo "'use strict'
+
+const chai = require('chai');
+const assert = chai.assert; // we are using the 'assert' style of Chai
+
+describe('Testcase Name', function () {
+
+  it('Test name', function () {
+    assert.isTrue(false); //make it fail initially
+  });
+
+});" >> test/test.js
+
 
 echo 'All done!'
 echo 'To run tests:  cd' $1 '&& npm test'
