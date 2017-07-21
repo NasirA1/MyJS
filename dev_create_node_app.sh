13530e4f341ad61b026e0459db9dd6418847134d
NasirA1 <pusarlai@msn.com>
2017-07-21 20:24:24 +0100

    blah

--- dev_create_node_app.sh
+++ dev_create_node_app.sh
@@ -17,7 +17,7 @@ echo 'initialising node app'
 npm init -y
 
 echo 'installing test frameworks'
-npm install mocha chai sinon --save-dev
+npm install mocha chai sinon sinon-stub-promise --save-dev
 
 echo 'Update "test" path in package.json'
 sed -i 's/"test": "echo \\"Error: no test specified\\" && exit 1"/"test": ".\/node_modules\/.bin\/mocha --reporter spec"/g' package.json
@@ -26,6 +26,9 @@ echo "'use strict'
 
 const chai = require('chai');
 const assert = chai.assert; // we are using the 'assert' style of Chai
+const sinonStubPromise = require('sinon-stub-promise');
+sinonStubPromise(sinon);
+
 
 describe('Testcase Name', function () {
 
@@ -38,5 +41,3 @@ describe('Testcase Name', function () {
 
 echo 'All done!'
 echo 'To run tests:  cd' $1 '&& npm test'
-
-
