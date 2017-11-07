c6c308a9eb79f7ffc6fd001e0073b96b7d1b1f71
Ahmad Nasir <nasir.ahmad@helvar.com>
2017-11-07 23:17:59 +0000

    added react-table

--- react-contacts-2/contacts-client/src/App.js
+++ react-contacts-2/contacts-client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Route, Link, NavLink, withRouter } from 'react-router-dom';
-import {Nav, Navbar, NavItem, Jumbotron, Button} from 'react-bootstrap';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import { Nav, Navbar, NavItem, Jumbotron, Button, PageHeader,
+         Table } from 'react-bootstrap';
 
 
 class Home extends Component {
@@ -8,11 +11,11 @@ class Home extends Component {
     return (
       <div>
       <Jumbotron>
-         <h1>Hello, world!</h1>
-         <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-         <p><Button bsStyle="primary">Learn more</Button></p>
-       </Jumbotron>      
-      <h3>Home</h3>
+         <h1>Welcome!</h1>
+         <p>Contacts Manager is a simple web application for managing contacts.</p>
+         <p><Link to="/browse"><Button bsStyle="primary">Browse Records</Button></Link></p>
+       </Jumbotron>
+     {/*<PageHeader>Home</PageHeader>*/}
       </div>
     );
   }
@@ -21,8 +24,75 @@ class Home extends Component {
 
 class Browse extends Component {
   render() {
+    const columns = [
+      {Header: 'ID', accessor: 'id', Cell: props => <span className='number'>{props.value}</span>},
+      {Header: 'First Name', accessor: 'firstName'},
+      {Header: 'Last Name', accessor: 'lastName'},
+      {Header: 'Email', accessor: 'email'},
+      {Header: 'Phone', accessor: 'phone'},
+      {Header: 'Address', accessor: 'address'}
+    ];
+    const data = [
+      {
+        id: '13',
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@gmail.com',
+        phone: '07945342233',
+        address: '23 Main Road'
+      },
+      {
+        id: '2',
+        firstName: 'Alice',
+        lastName: 'Wonderland',
+        email: 'alice.wonder@gmail.com',
+        phone: '',
+        address: '55 Maiden Avenue'
+      },
+      {
+        id: '6',
+        firstName: 'Martin',
+        lastName: 'Chol',
+        email: 'mrchol@yahoo.com',
+        phone: '0231223221',
+        address: '34 Balmoral Road'
+      },
+      {
+        id: '4',
+        firstName: 'Sumbyla3l',
+        lastName: 'Sotey',
+        email: 'sotey343@msn.com',
+        phone: '3222322322',
+        address: '23 Main Road'
+      },
+      {
+        id: '34',
+        firstName: 'Mulla',
+        lastName: 'Nasruddin',
+        email: 'mullakhana@gmail.af',
+        phone: '1111111111',
+        address: 'Karte Charrr'
+      },
+      {
+        id: '12',
+        firstName: 'Andrew',
+        lastName: 'Cole',
+        email: 'coleandrewww@mail.com',
+        phone: '55334343343',
+        address: 'Shshshshshsss'
+      }
+    ];
     return (
-      <h3>Browse</h3>
+      <div class="content">
+        <PageHeader>Browse</PageHeader>
+        <ReactTable
+          data={data} columns={columns}
+          defaultPageSize={5}
+          filterable={true}
+          onFetchData={() => null}
+          responsive
+        />
+      </div>
     );
   }
 }
@@ -31,7 +101,7 @@ class Browse extends Component {
 class Login extends Component {
   render() {
     return (
-      <h3>Login</h3>
+      <PageHeader>Login</PageHeader>
     );
   }
 }
@@ -39,7 +109,7 @@ class Login extends Component {
 class Register extends Component {
   render() {
     return (
-      <h3>Register</h3>
+      <PageHeader>Register</PageHeader>
     );
   }
 }
