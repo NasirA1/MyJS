608e304837e899b98a48206ef48bfd2922d3804c
Nasir Ahmad <nasir.ahmad@helvar.com>
2017-11-22 13:10:30 +0000

    amended home screen

--- react-contacts-2/contacts-client/src/components/Home.js
+++ react-contacts-2/contacts-client/src/components/Home.js
@@ -9,10 +9,9 @@ class Home extends Component {
       <div className="container">
         <Jumbotron>
           <h1>Welcome!</h1>
-          <p>Contacts Manager is a simple multi-user web application for managing contacts.  Please <Link to="/login">login</Link> or 
-          <Link to="/register">register</Link> to start using the system.
-           </p>
-          <p><Link to="/browse"><Button bsStyle="primary">Browse Records</Button></Link></p>
+          <p>Contacts Manager is a simple multi-user web application for managing contacts.  
+            Please <Link to="/login">login</Link> or <Link to="/register">register</Link> to start using the system.
+          </p>
         </Jumbotron>
       </div>
     );
