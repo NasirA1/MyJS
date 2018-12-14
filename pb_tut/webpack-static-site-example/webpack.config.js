75fb7d211b154fa07dca5a1d7d4271d01e653e62
Nasir <NasirA1@users.noreply.github.com>
2018-12-14 09:47:37 +0000

    setup webpack skeleton

---
+++ pb_tut/webpack-static-site-example/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/js/app.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Development'
+        })
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                loaders: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.ttf$/,
+                loaders: [
+                    'url-loader'
+                ]
+            },
+            {
+                test: /\.(svg|gif|png|eot|woff|ttf)$/,
+                loaders: [
+                    'url-loader'
+                ]
+            }
+        ]
+    }
+}
