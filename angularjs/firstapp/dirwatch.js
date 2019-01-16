a0ba5cd2e4fd7e62b786292345473d155d33f7a0
Nasir <NasirA1@users.noreply.github.com>
2019-01-16 17:02:42 +0000

    added filewatcher

---
+++ angularjs/firstapp/dirwatch.js
@@ -0,0 +1,10 @@
+var dirContent = ["dirwatch.exe","dirwatch.js","fourth.html","index.html","multi-ctrl.html","multi-view.html","node_modules","package-lock.json","package.json","second.html","third.html"];
+
+document.write("<div id='dirwatcher'>");
+dirContent.forEach(item => {
+  if(item.endsWith(".htm") || item.endsWith(".html"))
+    document.write("<a href='" + item + "'>" + item + "</a>&nbsp;&nbsp;")
+});
+
+document.write("<br /><hr /><br /></div>");
+
