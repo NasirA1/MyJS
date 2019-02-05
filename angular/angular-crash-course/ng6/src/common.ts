f358fbb2b7d3fb69846fd75de9d6c4cba67e3d65
Nasir <NasirA1@users.noreply.github.com>
2019-02-05 14:31:25 +0000

    ng6 tut

---
+++ angular/angular-crash-course/ng6/src/common.ts
@@ -0,0 +1,19 @@
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
+
+
+export const common = {
+  listStagger:
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(':enter', [
+          style({ opacity: 0, transform: 'translateY(-15px)' }),
+          stagger('100ms',
+            animate('1000ms ease-out',
+              style({ opacity: 1, transform: 'translateY(0px)' })
+            )
+          )
+        ], { optional: true }),
+        query(':leave', animate('50ms', style({ opacity: 0 })), { optional: true })
+      ])
+    ]) //end of listStagger
+};
